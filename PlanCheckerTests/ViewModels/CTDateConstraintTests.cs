f25e8b3a6537ffb09e0d02a4521101f6c268f9f9
Jimmy Jones <jimmy.jones.513@gmail.com>
2019-02-13 14:08:26 -0800

    Final CT with unit test

---
+++ PlanCheckerTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cardan.PlanChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Facade.API;
+using ESAPIX.Constraints;
+
+namespace Cardan.PlanChecker.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstrainPassesTest()
+        {
+            var im = new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-59); //59 days ago
+            var expected = ResultType.PASSED;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConstrainFailsTest()
+        {
+            var im = new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-61); //61 days ago
+            var expected = ResultType.ACTION_LEVEL_3;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
